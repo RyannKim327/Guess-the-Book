df8fdb0633f03c6f4bc5dea32fc24cb09e60c18d
RyannKim327 <rksesgundo123@gmail.com>
2022-06-24 00:36:12 +0800

    06-24-22 00:36

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const books = [
     "Joshua", "Judges", "Ruth", "1 Samuel", "2 Samuel",
     "1 Kings", "2 Kings", "1 Chronicles", "2 Chronicles", "Ezra",
     "Nehemiah", "Esther", "Job", "Psalms", "Proverbs",
-    "Eclesiates", "Song of Solomon", "Isiah", "Jeremiah", "Lamentations",
+    "Ecclasiastes", "Song of Solomon", "Isiah", "Jeremiah", "Lamentations",
     "Ezekiel", "Daniel", "Hosea", "Joel", "Amos",
     "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk",
-    "Zephaniah", "Haggai", "Zachariah", "Melachi"
+    "Zephaniah", "Haggai", "Zechariah", "Melachi"
   ],
   [
     "Matthew", "Mark", "Luke", "John", "Acts",
-    "Romans", "1 Corinthians", "2 Corinthians", "Galatians", "Ephisians",
-    "Philippians", "Colossians", "1 Tessalonians", "2 Tessalonians", "1 Timothy",
+    "Romans", "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians",
+    "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians", "1 Timothy",
     "2 Timothy", "Titus", "Philemon", "Hebrews", "James",
     "1 Peter", "2 Peter", "1 John", "2 John", "3 John",
     "Jude", "Revelation"
